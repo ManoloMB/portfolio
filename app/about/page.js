032a542769f4804bf073ba42dd920994a31f227b
ManoloMB <manolomartinezbernal@gmail.com>
2024-08-11 18:27:44 +0200

    moviendo de carpeta

--- app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">Sobre mí</h1>
-      <p className="mt-4 text-lg">Esta es la página sobre mí. Aquí puedes añadir una breve biografía.</p>
-    </div>
-  );
-}
