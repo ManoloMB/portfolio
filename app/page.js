e4e5e81b14acdcf1d1b54c26cc801afce5ae7d8a
ManoloMB <manolomartinezbernal@gmail.com>
2024-08-11 18:26:44 +0200

    añadiendo nuevo componente

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Navbar from './navbar';
-import About from './about';
-import Tecnology from './tecnology';
-import Projects from './projects';
-import Experience from './experience';
-import Footer from './footer';
-import data from './data.json';
+import Navbar from '@/components/navbar';
+import About from '@/components/about';
+import Tecnology from '@/components/tecnology';
+import Projects from '@/components/projects';
+import Experience from '@/components/experience';
+import ActualProjects from '@/components/actualProjects';
+import Footer from '@/components/footer';
+import data from '@/components/data.json';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Projects items={data.projectsItems} />
         <Tecnology items={data.tecnologyItems}/>
         <Experience items={data.experienceItems} />
+        <ActualProjects items={data.actualProjectsItems} />
       </main>
       <Footer />
     </div>
